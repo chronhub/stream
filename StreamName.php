07ac6879d9bf73db8cfb1a61d778f97dc8a1008f
steph bug <chronhubgit@gmail.com>
2023-05-24 14:21:34 +0200

    some work

--- StreamName.php
+++ StreamName.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 
 namespace Storm\Stream;
 
+use InvalidArgumentException;
 use Stringable;
-use function assert;
 
 final class StreamName implements Stringable
 {
     public function __construct(public string $name)
     {
-        assert($this->name !== '', 'Stream name given can not be empty');
+        if ($this->name === '') {
+            throw new InvalidArgumentException('Stream name given can not be empty');
+        }
     }
 
     public function __toString(): string
