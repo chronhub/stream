c13317b75be4bc4f12d90e2bf24fa198ee4ce579
steph bug <chronhubgit@gmail.com>
2023-08-31 20:56:53 +0200

    work(package): wip

--- Stream.php
+++ Stream.php
@@ -13,7 +13,7 @@ final class Stream
     private StreamEvents $events;
 
     public function __construct(
-        public readonly StreamName $streamName,
+        public readonly StreamName $name,
         iterable $events = []
     ) {
         $this->events = new StreamEvents($events);
@@ -21,7 +21,7 @@ final class Stream
 
     public function name(): StreamName
     {
-        return $this->streamName;
+        return $this->name;
     }
 
     public function events(): Generator
